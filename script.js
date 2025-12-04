7d8f41dbc1d23b0d2898b8b9364187e42e96bdee
juscas-777 <juscas777@gmail.com>
2025-12-03 18:57:26 -0800

    Layout/Columns/someJavascript

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const url =
   "https://api.airtable.com/v0/appd2bG7WHqaMZuiX/Table%201?maxRecords=3&view=Grid%20view";
 // function for our list view
 async function getAllRecords() {
-  //let getResultElement = document.getElementById("brews");
+  let getResultElement = document.getElementById("brews");
 
   const options = {
     method: "GET",
@@ -17,7 +17,31 @@ async function getAllRecords() {
     .then((response) => response.json())
     .then((data) => {
       console.log(data); // response is an object w/ .records array
+
+      getResultElement.innerHTML = ""; // clear brews
+
+      let newHtml = "";
+
+      for (let i = 0; i < data.records.length; i++) {
+        let image = data.records[i].fields["Image"]; // here we are getting column values
+        let poi = data.records[i].fields["POIs"]; //here we are using the Field ID to fecth the name property
+        let description = data.records[i].fields["Description"];
+
+        newHtml += `
+        
+         <div class="card" style="width: 18rem;">
+          <a href="" img src="${image[0].url}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${poi}</h5>
+            <p class="card-text">${description}</p>
+              <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+    
+        
+        `;
+      }
+
+      getResultElement.innerHTML = newHtml;
     });
 }
-//patkwXt1bmB9NocbI.2b2a3d222bc5c0de05595653a9c2fede447921fd0718fb14611729909c8aa5ba
-getAllRecords();
